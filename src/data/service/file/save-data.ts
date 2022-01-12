82484d7c32c08017f3edc4aef094f604e01fe34f
Breno Machado <bamachadu@gmail.com>
2022-01-12 13:53:10 -0300

    init open and extract data of the file

--- src/data/service/file/save-data.ts
+++ src/data/service/file/save-data.ts
@@ -4,6 +4,19 @@ export class SaveDataService {
   async save(arr: string[]): Promise<void> {
     console.log(9)
     console.log(arr)
-    
+
+    let f = []
+
+    for (let i of arr) {
+      const ini3 = '["' + i + '"]'
+      const ini4 = { obj: ini3.replace('\r', '') }
+
+      const a = Object.assign({}, JSON.parse(ini4.obj));
+
+      f.push(a)
+    }
+
+    console.log('f', f)
+
   }
 }
