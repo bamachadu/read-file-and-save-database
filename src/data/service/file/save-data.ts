6260d7e58afba5340b9587276bd448161e9c95ff
Breno Machado <bamachadu@gmail.com>
2022-01-13 11:59:03 -0300

    refact save data service to cleanup

--- src/data/service/file/save-data.ts
+++ src/data/service/file/save-data.ts
@@ -5,24 +5,20 @@ export class SaveDataService {
     private repository: SaveDataOfTheFileContract
   ) { }
 
-  async save(arr: string[], fileName:  string): Promise<void> {
-    let f = []
+  async save(arr: string[], fileName: string): Promise<void> {
+    let linesContent = []
 
     for (let i of arr) {
-      const ini3 = '["' + i + '"]'
-      const ini4 = { obj: ini3.replace('\r', '') }
+      const transformArrayObjectContent = Object.assign({}, JSON.parse({ obj: ('["' + i + '"]').replace('\r', '') }.obj));
 
-      const a = Object.assign({}, JSON.parse(ini4.obj));
-
-      f.push(a)
+      linesContent.push(transformArrayObjectContent)
     }
 
-    const props = {
-      fileName: fileName
-    }
+    const props = linesContent.map(lineContent => ({
+      content: lineContent,
+      fileName,
+    }))
 
     await this.repository.save(props)
-    console.log('f', f)
-
   }
 }
